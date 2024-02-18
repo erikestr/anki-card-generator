71ebf0c1bdfabb4cecf4d090803e5faf968f458f
Erik E <erikestrada37@outlook.com>
2024-02-18 17:40:50 -0600

    1.0.240218.a.0001 [master] To generate card

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,28 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [title, setTitle] = useState('')
+
+  const handleTitle = (event: any) => {
+    setTitle(event.target.value);
+  }
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1 className='es-text'>Title</h1>
+        <span className='es-input-container'>
+          <input className='es-input' type="text" onChange={handleTitle}/>
+        </span>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      
+      <div>
+        <h1 className='es-text'>Answer</h1>
+        <span className='es-input-container'>
+          <textarea className='es-text-area' rows={10} spellCheck='false'></textarea>
+        </span>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
